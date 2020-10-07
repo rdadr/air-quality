e16aeeb1c8a3cdadaf7b62d2fe926b109d1537e2
Sergii Sergiienko <66369690+rdadr@users.noreply.github.com>
2020-10-07 19:15:37 +0300

    Updates

---
+++ precache-manifest.0e743871ae1ee0247dc93d553d4781ac.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87729d162bda3701a3326d0284d4611",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "ac61ef5152aee5d7e55f",
+    "url": "/air-quality/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "13a17c6f30db8396ae05",
+    "url": "/air-quality/static/css/main.1d72fe4d.chunk.css"
+  },
+  {
+    "revision": "ac61ef5152aee5d7e55f",
+    "url": "/air-quality/static/js/2.a8513620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air-quality/static/js/2.a8513620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a17c6f30db8396ae05",
+    "url": "/air-quality/static/js/main.35957e33.chunk.js"
+  },
+  {
+    "revision": "be8b00b45510cbbb35d0",
+    "url": "/air-quality/static/js/runtime-main.2c0b8cd0.js"
+  },
+  {
+    "revision": "e60be19e8cbfbd550ac9a3f905ad359a",
+    "url": "/air-quality/static/media/GC_logo.e60be19e.svg"
+  },
+  {
+    "revision": "899cd2784349ab5b5a24f5dcee2ffca3",
+    "url": "/air-quality/static/media/ITN_logo.899cd278.svg"
+  },
+  {
+    "revision": "43f9dac62ffb9209e823b91b7504c5ec",
+    "url": "/air-quality/static/media/energetics.43f9dac6.jpg"
+  },
+  {
+    "revision": "fb084e1e416e340515649cb48eb04f6e",
+    "url": "/air-quality/static/media/energy.fb084e1e.jpg"
+  },
+  {
+    "revision": "5d86912d3747f4d2fb2419bd7d2de74b",
+    "url": "/air-quality/static/media/hero-bg.5d86912d.jpg"
+  },
+  {
+    "revision": "5ec44869469e2f3c9db3551c44662f88",
+    "url": "/air-quality/static/media/industry.5ec44869.jpg"
+  },
+  {
+    "revision": "8dd4ef7149a55ade70696aa0e9e2faa3",
+    "url": "/air-quality/static/media/shid_emozhl_logo.8dd4ef71.svg"
+  },
+  {
+    "revision": "f8f67844654cd9fc78dd57834ecfed6c",
+    "url": "/air-quality/static/media/sity_planing.f8f67844.jpg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/air-quality/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/air-quality/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/air-quality/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/air-quality/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/air-quality/static/media/slide-5.bb9f49bb.jpg"
+  },
+  {
+    "revision": "65643b05446e10934c8778999860e9de",
+    "url": "/air-quality/static/media/transport.65643b05.jpg"
+  },
+  {
+    "revision": "a8b6caeec11d336276f5ed05a63fc938",
+    "url": "/air-quality/static/media/utilisation.a8b6caee.jpg"
+  }
+]);
